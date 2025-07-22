6df4722dc672c096a7734a8803c825e8a4d8dd34
Guillaume (Goliver) Golay <goliworks@gmail.com>
2025-07-22 17:33:16 +0200

    Some text on the homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/Goliworks/Quark",
         },
       ],
       sidebar: [
